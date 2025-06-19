dc23f99ea870479d23c6374ab4cda1cfaaf8ffe6
pooyan <thepooyan@gmail.com>
2025-06-19 19:53:19 +0330

    nothing!

--- src/routes/(header)/Panel.tsx
+++ src/routes/(header)/Panel.tsx
@@ -4,7 +4,7 @@ import TA from "~/components/parts/TA"
 const Panel = ({children}:ParentProps) => {
   return (
     <main >
-      <div class=" border-1 border-zinc-300 m-4 rounded ">
+      <div class=" border-1 m-4 rounded w-max mx-auto overflow-hidden ">
         <I href="/Panel">داشبورد</I>
         <I href="/Panel/Profile">پروفایل</I>
       </div>
@@ -17,8 +17,8 @@ const Panel = ({children}:ParentProps) => {
 
 const I = ({href, children}:ParentProps<{href: string}>) => {
   return <TA href={href}
-    class="px-4 py-2 hover:bg-zinc-800 inline-block"
-    activeClass="bg-zinc-600"
+    class="px-5 py-3 inline-block"
+    activeClass="bg-zinc-800"
   >
     {children}
   </TA>
