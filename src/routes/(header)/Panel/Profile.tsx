92fe4ac2d151535a7e5cdede00175a7c50eec576
pooyan <thepooyan@gmail.com>
2025-06-20 14:43:28 +0330

    nothing!

--- src/routes/(header)/Panel/Profile.tsx
+++ src/routes/(header)/Panel/Profile.tsx
@@ -1,9 +1,58 @@
+import { FiPhone } from "solid-icons/fi"
+import { Button } from "~/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card"
+import Input from "~/components/ui/input"
+import { Label } from "~/components/ui/label"
 import { pageMarker } from "~/lib/routeChangeTransition"
 
 const Profile = () => {
   return (
     <main {...pageMarker()}>
-      Profile
+     <div class="flex justify-center">
+      <Card class="w-full max-w-2xl">
+        <CardHeader class="text-right">
+          <CardTitle class="text-2xl font-bold">فرم اطلاعات شخصی</CardTitle>
+          <CardDescription>در صورت تمایل اطلاعات شخصی خود را کامل کنید</CardDescription>
+        </CardHeader>
+        <form  >
+          <CardContent class="space-y-6">
+
+            <div class="space-y-2">
+              <Label for="name" class="block text-right">
+                  نام
+              </Label>
+              <div class="flex items-center">
+                <Input  placeholder="نام خود را وارد کنید" class="text-right" />
+              </div>
+            </div>
+
+            <div class="space-y-2">
+              <Label for="fax" class="block text-right">
+                  نام خانوادگی
+              </Label>
+              <div class="flex items-center">
+                <Input  placeholder="نام خانوادگی خود را وارد کنید" class="text-right" />
+              </div>
+            </div>
+
+            <div class="space-y-2">
+              <Label for="phone" class="block text-right">
+                شماره تلفن
+              </Label>
+              <div class="flex items-center">
+                <Input  placeholder="شماره تلفن را وارد کنید" class="text-right" />
+                <FiPhone class="w-5 h-5 text-gray-400 -mr-8" />
+              </div>
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button type="submit" class="w-full">
+              ثبت اطلاعات
+            </Button>
+          </CardFooter>
+        </form>
+      </Card>
+    </div>
     </main>
   )
 }
