25dcce39a85dc50f0c41f36131746096cb075acf
pooyan <thepooyan@gmail.com>
2025-06-24 19:12:41 +0330

    replaced sqlite with turso

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,10 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   out: './drizzle',
   schema: './src/db/schema.ts',
-  dialect: 'sqlite',
+  dialect: 'turso',
   dbCredentials: {
-    url: process.env.DB_FILE_NAME!,
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!
   },
 });
 
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/libsql';
 
-export const db = drizzle(process.env.DB_FILE_NAME!);
+export const db = drizzle({connection: {
+  url: process.env.TURSO_DATABASE_URL as string,
+  authToken: process.env.TURSO_AUTH_TOKEN as string
+}});
 
