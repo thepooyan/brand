bab28786c150578e559839bdb0f1bebf2f0605b3
pooyan <thepooyan@gmail.com>
2025-07-08 20:08:19 +0330

    nothing!

--- src/routes/api/chat.ts
+++ src/routes/api/chat.ts
@@ -1,5 +1,6 @@
 import { google } from '@ai-sdk/google';
 import { streamText } from 'ai';
+import sp from "./systemPrompt.json"
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -9,6 +10,7 @@ export async function POST({request}:{request: Request}) {
 
   const result = streamText({
     model: google('gemini-2.5-flash'),
+    system: sp.content,
     messages,
   });
 
