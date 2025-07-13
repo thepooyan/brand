152f88b9a814bc531b98bb0bdde85ecf96f81579
pooyan <thepooyan@gmail.com>
2025-07-13 17:45:37 +0330

    telegram error handleing

--- src/server/telegram.ts
+++ src/server/telegram.ts
@@ -3,32 +3,34 @@
 import { db } from "~/db/db"
 import { adminsTable } from "~/db/schema"
 
-const getadminToken = () => process.env.ADMIN_BOT!
-const getsupportToken = () => process.env.SUPPORT_BOT!
+const adminToken = process.env.ADMIN_BOT ?? (() => { throw new Error('Missing ADMIN_BOT') })()
+const supportToken = process.env.SUPPORT_BOT ?? (() => { throw new Error('Missing SUPPORT_BOT') })()
 
-const send = (token: string, text: string, chat_id: string) => {
+const send = async (token: string, text: string, chat_id: string | number) => {
   const url = `https://api.telegram.org/bot${token}/sendMessage`
-   return fetch(url, {
+  const res = await fetch(url, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ chat_id, text }),
   })
+
+  if (!res.ok) {
+    console.error(`Telegram error: ${res.statusText}`, await res.text())
+  }
+
+  return res
 }
 
 const sendToAdmin = async (text: string) => {
   let admins = await db.select().from(adminsTable)
-  admins.forEach(a => send(getadminToken(), text, a.chat_id))
+  await Promise.all(admins.map(a => send(adminToken, text, a.chat_id)))
 }
 
-const sendToSupport = (text: string, chat_id: string) => {
-  return send(getsupportToken(), text, chat_id)
+const sendToSupport = (text: string, chat_id: string | number) => {
+  return send(supportToken, text, chat_id)
 }
 
 export const telegram = {
-  admin: {
-    send: sendToAdmin
-  },
-  support: {
-    send: sendToSupport
-  }
+  admin: { send: sendToAdmin },
+  support: { send: sendToSupport },
 }
