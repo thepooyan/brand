04a5e476d1941ee2765a7b254802afd0d1e84719
pooyan <thepooyan@gmail.com>
2025-08-11 14:59:54 +0330

    nothing!

--- src/animation/primitives/Capsule.tsx
+++ src/animation/primitives/Capsule.tsx
@@ -2,6 +2,7 @@ import { AnimationOptions, DOMKeyframesDefinition } from "motion"
 import Motion from "./Motion"
 import { ParentProps } from "solid-js"
 
+
 export const Capsule = (animate: DOMKeyframesDefinition, options: AnimationOptions) => {
   return (pr:ParentProps) => {
     return <Motion animate={animate} options={options}>
