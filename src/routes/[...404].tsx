f01b53d7c50267468c892a189ebd68b16fbb8ff2
pooyan <thepooyan@gmail.com>
2025-10-02 11:58:33 +0330

    fix 404

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,13 +1,16 @@
 import { HttpStatusCode } from "@solidjs/start";
-import Spinner from "~/components/parts/Spinner";
+import TA from "~/components/parts/TA";
+import { Button } from "~/components/ui/button";
 
 export default function NotFound() {
   return (
     <>
       <HttpStatusCode code={404} />
       <div class="h-dvh flex flex-col justify-center items-center gap-4">
-        لطفا صبر کنید
-        <Spinner reverse/>
+        متاسفانه صفحه مورد نظر یافت نشد!
+        <div>
+          <Button variant="secondary" as={TA} href="/">خانه</Button>
+        </div>
       </div>
     </>
   );
