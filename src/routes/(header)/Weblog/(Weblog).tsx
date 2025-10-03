36893b9aa9a38251990f353faec7de277418422a
pooyan <thepooyan@gmail.com>
2025-10-03 18:04:36 +0330

    nothing!

--- src/routes/(header)/Weblog/(Weblog).tsx
+++ src/routes/(header)/Weblog/(Weblog).tsx
@@ -1,5 +1,7 @@
 import { createAsync } from "@solidjs/router"
 import { Suspense } from "solid-js"
+import BlogNotFound from "~/components/pages/blog/BlogNotFound"
+import NoBlogsYet from "~/components/pages/blog/NoBlogsYet"
 import BlogCard from "~/components/parts/BlogCard"
 import { Loading } from "~/components/parts/Loading"
 import { getAllBlogs } from "~/lib/queries"
@@ -11,6 +13,7 @@ const Weblog = () => {
   return (
     <>
       <Suspense fallback={<Loading/>}>
+        {blogs()?.length === 0 && <NoBlogsYet/>}
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 lg:p-10 pt-10 gap-5 w-max m-auto">
           {blogs()?.map(b => <BlogCard post={b} index={0}/> )}
         </div>
