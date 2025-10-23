62fa688644a905680be7a1e208e0631706a2e8e2
pooyan <thepooyan@gmail.com>
2025-10-23 15:13:47 +0330

    add robots.txt

---
+++ src/routes/robots.txt.tsx
@@ -0,0 +1,17 @@
+import type { APIEvent } from "@solidjs/start/server";
+
+export function GET({ request }: APIEvent) {
+  const baseUrl = new URL(request.url).origin;
+
+  const robotsTxt = `
+    User-agent: *
+    Allow: /
+    Sitemap: ${baseUrl}/sitemap.xml
+  `;
+
+  return new Response(robotsTxt.trim(), {
+    headers: {
+      "Content-Type": "text/plain",
+    },
+  });
+}
