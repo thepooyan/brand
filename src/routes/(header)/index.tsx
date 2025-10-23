4f90358838415242c95b8558bd25438f69e13031
pooyan <thepooyan@gmail.com>
2025-10-23 14:09:38 +0330

    nothing!

--- src/routes/(header)/index.tsx
+++ src/routes/(header)/index.tsx
@@ -3,10 +3,12 @@ import About from "~/components/landing/About";
 import { Contact } from "~/components/landing/Contact";
 import Hero from "~/components/landing/Hero";
 import Services from "~/components/landing/Services";
+import { Link } from "@solidjs/meta";
 
 export default function Home() {
   return (
     <div {...pageMarker()}>
+      <Link rel="canonical" href="https://hooshbaan.com/" />
       <Hero/>
       <Services/>
       <About/>
