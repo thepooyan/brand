8aa5f017a35bef524a722406f934e28c662760a0
pooyan <thepooyan@gmail.com>
2025-10-23 14:31:01 +0330

    nothing!

--- src/routes/(header)/ContactUs.tsx
+++ src/routes/(header)/ContactUs.tsx
@@ -1,9 +1,11 @@
 import { pageMarker } from "~/lib/routeChangeTransition"
 import { Contact } from "~/components/landing/Contact"
+import { Link } from "@solidjs/meta"
 
 const ContactUs = () => {
   return (
     <main {...pageMarker()}>
+      <Link rel="canonical" href="https://hooshbaan.com/ContactUs" />
       <Contact/>
     </main>
   )
