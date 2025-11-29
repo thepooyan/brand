6e93bde3a2cc35c7aff0770a5b3f66598a7520d3
pooyan <thepooyan@gmail.com>
2025-11-29 19:26:22 +0330

    nothing!

--- src/server/env/index.ts
+++ src/server/env/index.ts
@@ -1,6 +1,14 @@
 import { z } from "zod";
 import { envSchema } from "./envSchema";
+import { isServer } from "solid-js/web";
 
 export type Env = z.infer<typeof envSchema>;
 
 export const env: Env = envSchema.parse(process.env);
+export const getEnv = async () => {
+  console.log(isServer)
+  console.log(import.meta.env)
+  if (isServer) 
+  return envSchema.parse(process.env)
+  else return envSchema.parse(import.meta.env)
+}
