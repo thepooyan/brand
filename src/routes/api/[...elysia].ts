19b97121f2d8ee163fe61ce8634ef22c09abfe19
pooyan <thepooyan@gmail.com>
2025-12-06 15:55:59 +0330

    elysia works

--- src/routes/api/[...elysia].ts
+++ src/routes/api/[...elysia].ts
@@ -1,1 +1,18 @@
-export const GET = () => "hi"
+import { Elysia, t } from 'elysia'
+
+const app = new Elysia({ prefix: '/api' })
+    .get('/', 'Hello Nextjs')
+    .get("/folan", "hi folan")
+    .post('/', ({ body }) => body, {
+        body: t.Object({
+            name: t.String()
+        })
+    })
+
+const handle = ({ request }: { request: Request }) => app.handle(request);
+
+export const GET = handle;
+export const POST = handle;
+
+export type App = typeof app;
+
