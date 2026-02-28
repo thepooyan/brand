337a134681342afb6e34075fc8d3cbccbfcda658
pooyan <thepooyan@gmail.com>
2026-02-28 21:26:59 +0330

    nothing!

--- src/server/env/shared-env.ts
+++ src/server/env/shared-env.ts
@@ -12,9 +12,10 @@ export const sharedEnvSchema = z.object({
 
 export type sharedEnv = z.infer<typeof sharedEnvSchema>;
 export const getSharedEnv = () => sharedEnvSchema.parse(import.meta.env)
+export const sharedEnv = sharedEnvSchema.parse(import.meta.env)
 
 type featureSelector = (f: typeof Features) => Features
 export const featureEnabled = (f: featureSelector) => {
   const feature = f(Features)
-  return getSharedEnv().VITE_features.includes(feature)
+  return sharedEnv.VITE_features.includes(feature)
 }
