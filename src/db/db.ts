e86e3a4fdc96f6e8ec5cfdccb148b40baec10754
pooyan <thepooyan@gmail.com>
2026-02-28 20:24:03 +0330

    added local config and bisource db

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,10 +3,16 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { getEnv } from '~/server/env';
 import * as schem from "./schema"
 
-export const db = drizzle({connection: {
-  url: getEnv().TURSO_DATABASE_URL as string,
-  authToken: getEnv().TURSO_AUTH_TOKEN as string
-},
-  schema: schem
+export const db = drizzle({ 
+    connection: 
+      process.env.NODE_ENV === "production" ?
+      { 
+        url: getEnv().TURSO_DATABASE_URL as string,
+        authToken: getEnv().TURSO_AUTH_TOKEN as string
+      }
+      : 
+      {
+        url: "file:./mydb.sqlite"
+      },
+    schema: schem
 });
-
