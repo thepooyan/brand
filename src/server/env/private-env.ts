3b9dfe4715f6e03bb5e21e6a51182db07b73b1bc
pooyan <thepooyan@gmail.com>
2026-03-05 23:27:18 +0330

    add node env to private env

--- src/server/env/private-env.ts
+++ src/server/env/private-env.ts
@@ -13,6 +13,7 @@ export const privateEnvSchema = z.object({
   BUCKET_SECRET: z.string().min(1),
   BUCKET_NAME: z.string().min(1),
   SMS_PANEL: z.string().min(1),
+  NODE_ENV: z.literal(["development", "production"])
 });
 
 export type PrivateEnv = z.infer<typeof privateEnvSchema>;
