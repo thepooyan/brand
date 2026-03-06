7259bfafb0eb8179d3d6c47ce7ff08869fdb9ff5
pooyan <thepooyan@gmail.com>
2026-03-06 17:31:15 +0330

    add admin media update 1

--- src/routes/Admin/Media.tsx
+++ src/routes/Admin/Media.tsx
@@ -1,19 +1,27 @@
 import { createAsync } from "@solidjs/router"
-import { For, Show } from "solid-js"
+import { For, Show, Suspense } from "solid-js"
 import AdminImage from "~/components/parts/admin/AdminImage"
+import { Loading } from "~/components/parts/Loading"
+import { Button } from "~/components/ui/button"
 import { listS3Files } from "~/s3/s3Actions"
 
 const Media = () => {
   const files = createAsync(() => listS3Files())
 
   return (
-    <div class="grid grid-cols-3">
-      <Show when={files()}>
-        {f => <For each={f()}>
-          {i => <AdminImage url={i}/>}
-        </For>}
-      </Show>
-    </div>
+    <>
+      <Button class="m-5">افزودن</Button>
+
+      <Suspense fallback={<Loading/>}>
+        <div class="grid grid-cols-3">
+          <Show when={files()}>
+            {f => <For each={f()}>
+              {i => <AdminImage url={i}/>}
+            </For>}
+          </Show>
+        </div>
+      </Suspense>
+    </>
   )
 }
 
