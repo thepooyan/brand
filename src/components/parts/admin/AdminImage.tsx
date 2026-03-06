0e914cd872fbcfb46085b2bf73d59cb0922418f4
pooyan <thepooyan@gmail.com>
2026-03-06 17:35:54 +0330

    add admin media update 2

--- src/components/parts/admin/AdminImage.tsx
+++ src/components/parts/admin/AdminImage.tsx
@@ -1,23 +1,39 @@
 import { FaSolidBucket } from "solid-icons/fa"
 import { FiCopy } from "solid-icons/fi"
+import { callModal } from "~/components/layout/Modal"
 import { Button } from "~/components/ui/button"
+import Copyable from "~/components/ui/copyable"
+import { deleteFileFromS3 } from "~/s3/s3Actions"
 
 interface p {
   url: string
 }
 const AdminImage = ({url}:p) => {
+
+  const deleteImage = () => {
+    callModal.prompt("حذف شود؟")
+    .yes(async () => {
+        callModal.wait()
+        let res = await deleteFileFromS3(url)
+        if (res === null) callModal.fail("حذف موفقیت آمیز نبود.")
+        callModal.success("انجام شد!")
+      })
+  }
+
   return (
     <div class="p-10 hover:bg-muted rounded-lg transition-colors">
       <img src={url} class="rounded-lg h-60 w-60 m-auto object-contain"/>
 
       <div class="flex justify-center gap-3 mt-5">
-      <Button>
-        <FiCopy/>
-      </Button>
+        <Copyable toCopy={url}>
+          <Button> 
+            <FiCopy/>
+          </Button>
+        </Copyable>
 
-      <Button class="bg-destructive hover:bg-destructive/80" >
-        <FaSolidBucket/>
-      </Button>
+        <Button class="bg-destructive hover:bg-destructive/80" onclick={deleteImage}>
+          <FaSolidBucket/>
+        </Button>
       </div>
       
     </div>
