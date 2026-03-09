4d847256bbad7d737d7287d8ed111f0f02d5d136
pooyan <thepooyan@gmail.com>
2026-03-09 15:20:39 +0330

    nothing!

--- src/server/api_routers/chat/sessionChat.ts
+++ src/server/api_routers/chat/sessionChat.ts
@@ -2,7 +2,7 @@ import Elysia from "elysia";
 import { chatGaurd } from "./chatGuard";
 import { getAuthSession } from "~/lib/session";
 import { db } from "~/db/db";
-import { chatbotTable, chatbotStatusTable } from "~/db/schema";
+import { chatbotTable, planTable, usersTable } from "~/db/schema";
 import { and, eq, sql } from "drizzle-orm";
 import { streamText } from "ai";
 import { google } from "@ai-sdk/google";
@@ -34,27 +34,33 @@ export const sessionChatRouter = new Elysia({ prefix: "/session" })
 )
 
 const getUserBot = async (userId: string, botId: string ) => {
-  const bot = (await db.select()
-    .from(chatbotTable)
-    .leftJoin(chatbotStatusTable, eq(chatbotTable.id, chatbotStatusTable.id))
-    .where(
-      and(
+
+  const user = await db.query.usersTable.findFirst({
+    where: eq(usersTable.id, parseInt(userId)),
+    with: {current_plan: true}
+  })
+
+  const plan = user?.current_plan
+
+  if (!plan || !user.current_plan_id) return "404"
+
+  const bot = await db.query.chatbotTable.findFirst({
+    where: and(
         eq(chatbotTable.userId, parseInt(userId)),
         eq(chatbotTable.id, parseInt(botId))
-      )
-    )).at(0)
+    )
+  })
 
   if (!bot) return "404"
-  if (!bot.chatbot_status) return "404"
 
-  let remaining = bot.chatbot_status.remainingMessages
+  let remaining = plan.remainingMessages
   if (remaining <= 0) return "empty"
 
-  await db.update(chatbotStatusTable)
+  await db.update(planTable)
     .set({
-      remainingMessages: sql`${chatbotStatusTable.remainingMessages} - 1`
+      remainingMessages: sql`${planTable.remainingMessages} - 1`
     })
-    .where(eq(chatbotStatusTable.id, bot.chatbot.id))
+    .where(eq(planTable.id, user.current_plan_id ))
 
-  return bot.chatbot
+  return bot
 }
