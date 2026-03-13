1c244719650c389e9cf85de44bfa896f6742134b
pooyan <thepooyan@gmail.com>
2026-03-14 02:55:32 +0330

    nothing!

--- src/components/ticket/ticket-card.tsx
+++ src/components/ticket/ticket-card.tsx
@@ -1,9 +1,8 @@
 import { Ticket } from "~/db/schema"
 import { Button } from "../ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card"
-import { AiFillWarning, AiOutlineCheck } from "solid-icons/ai"
-import { For } from "solid-js"
-import { FiAlertCircle } from "solid-icons/fi"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card"
+import { AiFillWarning } from "solid-icons/ai"
+import { FiAlertCircle, FiCheck } from "solid-icons/fi"
 import { limitChar } from "~/lib/utils"
 
 interface p {
@@ -18,7 +17,7 @@ const TicketCard = ({t}:p) => {
         </CardTitle>
         <CardDescription class="flex gap-2 items-center">
           {t.state === "pending" && <AiFillWarning class="text-orange-500"/>}
-          {t.state === "responded" && <AiOutlineCheck class="text-green-500"/>}
+          {t.state === "responded" && <FiCheck class="bg-green-500 rounded-full text-foreground p-1"/>}
           وضعیت: 
           {t.state === "pending" && "در انتظار پاسخ"}
           {t.state === "responded" && "پاسخ داده شده"}
