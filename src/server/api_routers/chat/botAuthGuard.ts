2b8d998b5d92a049871176464689f3cb60067701
pooyan <thepooyan@gmail.com>
2026-03-17 21:37:44 +0330

    nothing!

--- src/server/api_routers/chat/botAuthGuard.ts
+++ src/server/api_routers/chat/botAuthGuard.ts
@@ -3,6 +3,7 @@ import Elysia from "elysia";
 import z from "zod";
 import { db } from "~/db/db";
 import { tokenLength } from "~/db/schema";
+import { isChatAllowed } from "~/server/botUtil";
 
 const AuthorizationHeader = z.object({
   authorization: z
@@ -20,13 +21,21 @@ export const botAuthGuard = new Elysia()
   headers: AuthorizationHeader,
 })
 .resolve({as: "scoped"}, async ({headers, status}) => {
-  const bot = await getBot(headers.token || "");
-  if (!bot) return status(403);
+  const token = headers.token
+  if (!token) return status(400, {errorMessage: "توکنی از سمت کاربر یافت نشد"})
+
+  const bot = await getBot(token);
+  if (!bot) return status(404, {errorMessage: "ربات یافت نشد"});
+
+  const res = isChatAllowed(bot.user)
+  if (!res.ok) return status(res.status, {errorMessage: res.msg} )
+
   return {bot: bot}
 })
 
 const getBot = (token: string) => {
   return db.query.chatbotTable.findFirst({
     where: (tbl) => eq(tbl.current_token, token),
+    with: {user: {with: {current_plan: true}}}
   });
 };
