49e435dc965d963210f2feabe7706f088f1c8006
pooyan <thepooyan@gmail.com>
2026-03-20 19:53:08 +0330

    nothing!

--- src/db/script.ts
+++ src/db/script.ts
@@ -1,4 +1,4 @@
 import { db } from "./db";
 import { adminsTable, chatbotTable, planTable, ticketTable } from "./schema";
 
-await db.update(chatbotTable).set({logo: null})
+await db.update(chatbotTable).set({floatingMessage: {msg: "سوال خود را از دستیار هوشمند بپرسید!", active: true}})
