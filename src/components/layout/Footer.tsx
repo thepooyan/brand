d9cb22c91881bc19bed20a38731a37e4828092cf
pooyan <thepooyan@gmail.com>
2026-03-25 21:09:58 +0330

    nothing!

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,7 +10,7 @@ import { isProd } from "~/server/env/shared-env"
 const Footer = () => {
   return (
     <>
-      <footer class="bg-black py-12">
+      <footer class="bg-background py-12">
         <div class="container mx-auto px-4">
           <div class="grid grid-cols-1 md:grid-cols-5 gap-8">
             <div>
