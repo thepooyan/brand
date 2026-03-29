5b67a2fca9058cf33fed7ddffd53d3812380fa67
pooyan <thepooyan@gmail.com>
2026-03-29 21:12:46 +0330

    nothing!

--- src/components/admin/user-card.tsx
+++ src/components/admin/user-card.tsx
@@ -3,6 +3,7 @@ import { Button } from "../ui/button"
 import { Muted } from "../prose/prose-item"
 import TA from "../parts/TA"
 import { User_Plan_Bots } from "~/db/schema"
+import { AiFillLock } from "solid-icons/ai"
 
 interface p {
   user: User_Plan_Bots
@@ -10,7 +11,10 @@ interface p {
 
 const UserCard = ({user}:p) => {
   return (
-    <Card class="grid grid-cols-[1fr_1fr_2fr_1fr_1fr_1fr] p-4 gap-1">
+    <Card class="grid grid-cols-[.2fr_1fr_2fr_1fr_1fr_1fr] p-4 gap-1 items-center">
+      <p>
+        {user.isBlocked === "1" && <AiFillLock class="text-destructive"/> }
+      </p>
       <p>
         <Muted>شماره:</Muted>
         {user.number}
@@ -18,10 +22,6 @@ const UserCard = ({user}:p) => {
       <p>
         <Muted>نام:</Muted>
         {user.name || "ندارد"}
-      </p>
-      <p>
-        <Muted>ایمیل:</Muted>
-        {user.email || "ندارد"}
       </p>
       <p>
         <Muted>تعداد پلن:</Muted>
