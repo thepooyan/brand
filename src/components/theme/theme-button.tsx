c1c55a1a7099588f80c51253ca243c328b16c5e2
pooyan <thepooyan@gmail.com>
2026-03-30 16:29:51 +0330

    nothing!

--- src/components/theme/theme-button.tsx
+++ src/components/theme/theme-button.tsx
@@ -2,10 +2,12 @@ import { FiMoon, FiSun } from "solid-icons/fi"
 import { Button } from "../ui/button"
 import { theme, toggleTheme } from "~/lib/theme"
 import { cn } from "~/lib/utils"
+import { createEffect } from "solid-js"
 
 const ThemeButton = () => {
 
   const isDark = () => theme() === "dark"
+  createEffect(() => console.log(isDark()))
 
   return (
     <Button variant="outline" onclick={() => toggleTheme()}
@@ -13,11 +15,11 @@ const ThemeButton = () => {
     >
       <FiSun class={cn(
         "transition-all absolute duration-100",
-        isDark() && "opacity-0 rotate-45 invisible"
+        !isDark() && "opacity-0 rotate-45 invisible"
       )}/>
       <FiMoon class={cn(
         "transition-all absolute duration-100",
-        !isDark() && "opacity-0 -rotate-45 invisible"
+        isDark() && "opacity-0 -rotate-45 invisible"
       )}/>
     </Button>
   )
