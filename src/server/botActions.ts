38ad1f32c0c908fd2ab62a442a977fd82982ef03
pooyan <thepooyan@gmail.com>
2026-03-30 18:00:22 +0330

    nothing!

--- src/server/botActions.ts
+++ src/server/botActions.ts
@@ -4,16 +4,16 @@ import { db } from "~/db/db"
 import { generateToken, hashToken } from "./serverUtil"
 import { eq } from "drizzle-orm"
 import { chatbotTable } from "~/db/schema"
-import { ActionResponse } from "~/lib/actionAbstraction"
-import { safe } from "~/lib/utils"
+import {  Fetch, fetchSuccess } from "~/lib/actionAbstraction"
+import { safeDb2 } from "~/lib/utils"
 
-export const getNewToken = async (id: number):ActionResponse<string> => {
+export const getNewToken = async (id: number):Fetch<string> => {
   const newToken = generateToken()
-  const data = await safe(
+  const data = await safeDb2(
     db.update(chatbotTable).set({current_token: hashToken(newToken)}).where(eq(chatbotTable.id, id))
   )
   if (data.ok) {
-    return {ok: true, data: newToken}
+    return fetchSuccess(newToken)
   }
-  return {ok: false, msg: "تولید توکن موفقیت آمیز نبود. لطفا مجددا تلاش کنید"}
+  return data
 }
