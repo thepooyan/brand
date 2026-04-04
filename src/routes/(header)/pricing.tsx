20ec0d05809900ef7996abf10400bf458b66ce8d
pooyan <thepooyan@gmail.com>
2026-04-04 17:39:15 +0330

    nothing!

--- src/routes/(header)/pricing.tsx
+++ src/routes/(header)/pricing.tsx
@@ -3,7 +3,7 @@ import { For } from "solid-js"
 import GenerallSelect from "~/components/parts/generall-select"
 import PlanCard from "~/components/plan/plan-card"
 import PlanSidebar from "~/components/plan/plan-sidebar"
-import { Muted } from "~/components/prose/prose-item"
+import { H3, Muted } from "~/components/prose/prose-item"
 import { selectedMounth, setSelectedMounth } from "~/components/plan/plan-signal"
 
 
@@ -12,12 +12,15 @@ const plans = () => {
     <div class="container">
       <h1 class="text-3xl mb-10 mt-10 font-bold text-center">قیمت‌ها</h1>
 
-      <div class="flex gap-2 items-center">
-        <Muted>
-          انتخاب مدت دوره:
-        </Muted>
-        <div class="w-50">
-          <M initialValue={selectedMounth()} onchange={e => setSelectedMounth(e)}/>
+      <div class="flex justify-between">
+        <H3>چت‌بات هوش مصنوعی:</H3>
+        <div class="flex gap-2 items-center">
+          <Muted>
+            انتخاب مدت دوره:
+          </Muted>
+          <div class="w-50">
+            <M initialValue={selectedMounth()} onchange={e => setSelectedMounth(e)}/>
+          </div>
         </div>
       </div>
       <div class="grid grid-cols-3 gap-3 p-5">
