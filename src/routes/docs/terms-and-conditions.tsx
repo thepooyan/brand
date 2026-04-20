d8d04dc8f49ad31510b6a71d07b0f72f877a9621
pooyan <thepooyan@gmail.com>
2026-04-20 13:19:03 +0330

    no

--- src/routes/docs/terms-and-conditions.tsx
+++ src/routes/docs/terms-and-conditions.tsx
@@ -1,14 +1,31 @@
 import {marked} from "marked"
 import md from "../../md/terms-and-conditions.md?raw"
+import { createSignal, onMount } from "solid-js"
+import { Dynamic } from "solid-js/web"
+import { Loading } from "~/components/parts/Loading"
+import { Button } from "~/components/ui/button"
 
 const TermsAndConditions = () => {
 
   const content = marked(md, {async: false})
+  let div!:HTMLDivElement
+
+  const [s, setS] = createSignal(<Loading/>)
+
+  onMount(() => {
+    let h = div.querySelectorAll("h2")
+    setS( [...h].map(m => <Button size="sm" variant="ghost" innerHTML={m.innerHTML} class="justify-start"
+      onclick={() => window.scrollTo({top: m.clientHeight})}
+    />) )
+  })
 
   return (
-    <>
-      <div innerHTML={content}></div>
-    </>
+    <div class="grid grid-cols-[5fr_2fr] gap-2">
+      <div innerHTML={content} ref={div}></div>
+      <div class="p-1 bg-sidebar rounded-md h-max sticky top-5  flex flex-col ">
+        {s()}
+      </div>
+    </div>
   )
 }
 
