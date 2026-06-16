efc69d11e50af42a4a58d2682f3bdb76d8f3d5a7
pooyan <thepooyan@gmail.com>
2026-06-16 17:27:25 +0330

    revert -- remove (header) group

--- src/routes/(header).tsx
+++ src/routes/(header).tsx
@@ -1,16 +1,9 @@
 import { RouteSectionProps } from "@solidjs/router"
-import { ErrorBoundary } from "solid-js"
-import Header from "~/components/layout/Header"
-import ErrorPage from "~/components/pages/ErrorPage"
 
 const header = (props: RouteSectionProps) => {
   return (
     <div class="min-h-screen flex flex-col relative" dir="rtl">
-      <Header/>
-      <ErrorBoundary fallback={e => <ErrorPage error={e}/>}>
-        {props.children}
-      </ErrorBoundary>
-      footer
+      {props.children}
     </div>
   )
 }
