1473f05de14401eb06ac48a68a3d6f46f1fb52c3
pooyan <thepooyan@gmail.com>
2026-06-16 17:22:26 +0330

    revert -- remove index content

--- src/routes/(header)/index.tsx
+++ src/routes/(header)/index.tsx
@@ -1,18 +1,9 @@
 import { pageMarker } from "~/lib/routeChangeTransition";
-import About from "~/components/landing/About";
-import { Contact } from "~/components/landing/Contact";
-import Hero from "~/components/landing/Hero";
-import Services from "~/components/landing/Services";
-import { Link } from "@solidjs/meta";
 
 export default function Home() {
   return (
     <div {...pageMarker()}>
-      <Link rel="canonical" href="https://hooshbaan.com/" />
-      <Hero/>
-      <Services/>
-      <About/>
-      <Contact/>
+      hi
     </div>
   )
 }
