f5253ff264e186cfddbf3f88fc578e52a9914d31
pooyan <thepooyan@gmail.com>
2026-06-16 18:10:03 +0330

    Revert "revert -- remove (header) group"
    This reverts commit efc69d11e50af42a4a58d2682f3bdb76d8f3d5a7.

--- src/routes/(header).tsx
+++ src/routes/(header).tsx
@@ -1,9 +1,16 @@
 import { RouteSectionProps } from "@solidjs/router"
+import { ErrorBoundary } from "solid-js"
+import Header from "~/components/layout/Header"
+import ErrorPage from "~/components/pages/ErrorPage"
 
 const header = (props: RouteSectionProps) => {
   return (
     <div class="min-h-screen flex flex-col relative" dir="rtl">
-      {props.children}
+      <Header/>
+      <ErrorBoundary fallback={e => <ErrorPage error={e}/>}>
+        {props.children}
+      </ErrorBoundary>
+      footer
     </div>
   )
 }
