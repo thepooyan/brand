61764fbf342e06e826c0095ec2f29da3ee369c6f
pooyan <thepooyan@gmail.com>
2026-06-16 17:32:24 +0330

    revert -- empty app

--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,17 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { ErrorBoundary, Suspense } from "solid-js";
-import "./app.css";
-import Modal from "./components/layout/Modal";
-import FallbackPage from "./components/pages/FallbackPage";
-import {Meta, MetaProvider, Title} from "@solidjs/meta"
-import ErrorPage from "./components/pages/ErrorPage";
-import { description, name, nameEn } from "../config/config";
-import { updateThemeSignal } from "./lib/theme";
+import { Suspense } from "solid-js";
+import { MetaProvider, Title} from "@solidjs/meta"
 
 export default function App() {
 
-  updateThemeSignal()
-
   return (
     <>
-      <Modal/>
       <Router
         root={props => (
           <MetaProvider>
-            <Title> {name} | {nameEn} </Title>
-            <Meta name="description" content={description}/>
-            <ErrorBoundary fallback={e=> <ErrorPage error={e}/>}>
-              <Suspense fallback={<FallbackPage/>}>{props.children}</Suspense>
-            </ErrorBoundary>
+            <Title> hi </Title>
+            <Suspense fallback={"..."}>{props.children}</Suspense>
           </MetaProvider>
         )}
       >
