0201de8179291e9311c9b4dd5e601125c3e80ce8
pooyan <thepooyan@gmail.com>
2026-06-16 18:10:29 +0330

    Revert "revert -- remove index content"
    This reverts commit 1473f05de14401eb06ac48a68a3d6f46f1fb52c3.

--- src/routes/(header)/index.tsx
+++ src/routes/(header)/index.tsx
@@ -1,9 +1,18 @@
 import { pageMarker } from "~/lib/routeChangeTransition";
+import About from "~/components/landing/About";
+import { Contact } from "~/components/landing/Contact";
+import Hero from "~/components/landing/Hero";
+import Services from "~/components/landing/Services";
+import { Link } from "@solidjs/meta";
 
 export default function Home() {
   return (
     <div {...pageMarker()}>
-      hi
+      <Link rel="canonical" href="https://hooshbaan.com/" />
+      <Hero/>
+      <Services/>
+      <About/>
+      <Contact/>
     </div>
   )
 }
